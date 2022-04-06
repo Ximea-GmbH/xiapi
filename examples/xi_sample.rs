c025b5bd6287688e1360f35afc8118e1b78e475c
Alexander Broemmer <alexander.broemmer@ximea.com>
2022-04-06 17:11:05 +0200

    Add first version of xi_sample
    The xi_sample example now opens the camera, starts the image acquisition and gets 10 images.

---
+++ examples/xi_sample.rs
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022. XIMEA GmbH - All Rights Reserved
+ */
+fn main() -> Result<(), i32> {
+    let mut cam = xiapi::open_device(None)?;
+
+    cam.set_exposure(10000.0)?;
+
+    let buffer = cam.start_acquisition()?;
+
+    for _ in 0..10 {
+        let image = buffer.next_image::<u8>(None)?;
+
+        // TODO: There should be a safe way to do this that just returns an error if out of bounds
+        let pixel = unsafe {
+            image.pixel(0, 0)
+        };
+        match pixel {
+            Some(&pixel_value) => println!("Image received, first pixel: {}", pixel_value),
+            None => unreachable!("Could not get pixel value from image!"),
+        }
+    }
+    Ok(())
+}
